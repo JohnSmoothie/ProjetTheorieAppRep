d449d16d3b799d61e1e093435a229e9ce9d4ea81
Victor BOIX <victor.boix@etu.univ-nantes>
2018-02-28 11:19:18 +0100

    debut de la configuration du serveur

--- src/serveur/serveur.go
+++ src/serveur/serveur.go
@@ -1,21 +1,38 @@
 package main
 
 import (
-
+  "fmt"
+  "net"
+  "bufio"
+  "strings"
+  "sync"
 )
 
 func main() {
+  fromCollector := make(chan string)
+  listener,err:=net.Listen("tcp",":10000")
+  if err!=nil{
+    fmt.Println("Erreur de Listen")
+  }
 
+  for {
+    connexion, err := listener.Accept()
+    if err!=nil{
+      fmt.Println("Erreur de Connexion")
+    }
+
+    go collecteur(connexion, fromCollector)
+  }
 }
 
-func collecteur() {
-
+func collecteur(connexion net.Conn, fromCollector chan string) {
+  
 }
 
 func repartiteur() {
 
 }
 
-func collecteur() {
-  
+func travailleur() {
+
 }
